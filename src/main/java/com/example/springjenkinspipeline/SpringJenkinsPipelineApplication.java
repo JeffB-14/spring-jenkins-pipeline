ef22aa492cbdfab4fbece17365b5fd910e7fb0a6
Jeff Beier <jeff.beier@cygrp.com>
2022-01-28 16:22:45 -0600

    initial commit

---
+++ src/main/java/com/example/springjenkinspipeline/SpringJenkinsPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.example.springjenkinspipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJenkinsPipelineApplication.class, args);
+	}
+
+}
